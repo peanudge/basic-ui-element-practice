3188d79c190ba901d3e8efa241bf81eda8bbb16e
peanut-lover <jihoson94@gmail.com>
2024-03-20 22:53:11 +0900

    Init

---
+++ src/components/08_scrollBox/react/index.tsx
@@ -0,0 +1,32 @@
+import ScrollBox from './scrollBox'
+import data from '../data'
+import { useRef } from 'react'
+import dynamic from 'next/dynamic'
+
+const LazyImage = dynamic(() => import('@/components/06_lazyLoading/4_r/lazyImage'), { ssr: false })
+
+export const Item = ({
+  id,
+  description,
+  imgUrl,
+}: {
+  id: string
+  description: string
+  imgUrl: string
+}) => (
+  <div>
+    <LazyImage src={imgUrl} width={250} height={400} />
+    <span>{description}</span>
+  </div>
+)
+
+const ScrollBox_React = () => {
+  const ref = useRef()
+  return (
+    <>
+      <h3>#1. React</h3>
+      <ScrollBox list={data} Item={Item} ref={ref} />
+    </>
+  )
+}
+export default ScrollBox_React
